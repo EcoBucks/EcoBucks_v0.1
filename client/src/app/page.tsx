b164e7e597b1bf9c354d952b6740c7c4e5f83bc4
adaoho <9nugroho@gmail.com>
2023-12-17 10:05:15 +0700

    update new branch

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -25,7 +25,6 @@ export default async function Home() {
   };
 
   const data: res = await fetchData();
-  // console.log(data, "=========");
 
   if (!data) {
     redirect("http://localhost:3000/login");
