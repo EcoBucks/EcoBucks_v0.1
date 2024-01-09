b0b1a4d897968776a7a642b9972a9cddd83bdea7
AKB4R03 <masakbar@gmail.com>
2024-01-09 11:01:22 +0700

    deploy 4

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["www.its.ac.id"],
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Alllow-Credentials", value: "true" },
+          { key: "Access-Control-Alllow-Origin", value: "*" },
+          {
+            key: "Access-Control-Alllow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Alllow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
